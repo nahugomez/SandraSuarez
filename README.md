31124a1571ebd35aaea6732729d47341b314c663
Nahuel Gomez <nahuelgomezs@outlook.com>
2022-03-12 12:13:17 -0300

    V1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# PAGINA WEB de SS ASESORIA Y GESTION CONTABLE
+Realizada por: Nahuel Gómez
+
+*V1.0*:
+- Estructura HTML (TERMINADO)
+- Diseño CSS (TERMINADO)
+- ScrollReveal (TERMINADO)
+- Etiquetas META, Información de la página y redirecciones (PENDIENTE)
---
+++ js/index.js
@@ -0,0 +1,45 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar-brand', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal('.navbar-toggler-icon', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal('.navbar-toggler', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal('.header-content', {
+    duration: 3000,
+});
+
+sr.reveal('.services', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px',
+});
+
+sr.reveal('.us', {
+    duration: 3000,
+});
+
+sr.reveal('.indblog', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px',
+});
+
+sr.reveal('.contact', {
+    duration: 3000,
+});
+
+
